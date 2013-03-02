4143ec753b36c0a638aafd59f40377fab28b0e80
Daiver <ra22341@ya.ru>
2013-03-02 14:45:39 +0400

    reformat code. tab=4 spaces

---
+++ src/SearchEngine.scala
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: kirill
+ * Date: 02.03.13
+ * Time: 14:43
+ * To change this template use File | Settings | File Templates.
+ */
+class SearchEngine {
+
+}
