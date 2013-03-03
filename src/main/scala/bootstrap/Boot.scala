af7b3a67cdfbf1246834f02dea1837fa0787269c
Yau Ref <yau.ref@gmail.com>
2013-03-04 00:39:59 +0400

    Home page

--- src/main/scala/bootstrap/Boot.scala
+++ src/main/scala/bootstrap/Boot.scala
@@ -6,12 +6,7 @@ import net.liftweb.sitemap.{Menu, SiteMap}
 class Boot {
   def boot {
     // where to search snippet
-    LiftRules.addToPackages("org.yourorganization.liftfromscratch")
-
-    // Build SiteMap
-    def sitemap(): SiteMap = SiteMap(
-      Menu.i("Home") / "index"
-    )
+    LiftRules.addToPackages("org.plex.googolplex")
 
     // Use HTML5 for rendering
     LiftRules.htmlProperties.default.set((r: Req) =>
