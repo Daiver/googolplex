93ce98a40fe21996c49c8fc9797fb59fddfbe67e
Yau Ref <yau.ref@gmail.com>
2013-03-07 22:43:39 +0400

    Created initial version of search page

---
+++ src/main/scala/org/plex/googolplex/Searcher.scala
@@ -0,0 +1,16 @@
+package org.plex.googolplex
+package snippet
+
+import net.liftweb.util._
+import Helpers._
+import net.liftweb.http.S
+
+class Searcher {
+
+  val request = S.param("r") openOr ""
+
+  def results = if(request == "") "No request" else "No results for " + request
+
+  def search = "#results" #> results
+}
+
