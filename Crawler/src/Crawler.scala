1bc5ce641d573ae02cd1852d11b1646484a14793
Yau Ref <yau.ref@gmail.com>
2013-03-10 23:57:29 +0400

    Lite cleaning of Crawler

--- Crawler/src/Crawler.scala
+++ Crawler/src/Crawler.scala
@@ -1,126 +1,105 @@
+import collection.mutable
+import io.Source
 import java.net.URL
 import java.security.MessageDigest
 import java.util.regex.Pattern
-import java.util.Date
-import ru.kolyvan.redis.Conv._
-import ru.kolyvan.redis.Redis
+import java.util.{StringTokenizer, Date}
 
-/**
- * Created with IntelliJ IDEA.
- * User: kirill
- * Date: 02.03.13
- * Time: 14:28
- * To change this template use File | Settings | File Templates.
- */
+class StoredPage(val url: String,
+                 val pageHtml: String,
+                 val keyWords: mutable.HashMap[String, Int],
+                 val links: List[String],
+                 val images: List[String],
+                 val hash: Array[Byte],
+                 val grabDate: Date) {
 
-case class StoredPage(URL: String,
-                      page_html: String,
-                      keyWords: scala.collection.mutable.HashMap[String, Int],
-                      links: List[String],
-                      images: List[String],
-                      hash: Array[Byte],
-                      grab_date: Date) {
-
-    def saveIntoDB(dbclient: Redis) = {
-        val already_exists = dbclient.keys("pages:*:URL:" + this.URL)
-        val index = if (already_exists.length > 0) already_exists(0).split(":")(1)
-                    else {
-            val res = S(dbclient.get("pages:globalindex")).get
-            dbclient.incr("pages:globalindex")
-            res
-        }
-        println("index:" + index)
-        val page_key = "pages:" + index + ":"
-        println(page_key + "URL:" + this.URL)
-        dbclient.set(page_key + "URL:" + this.URL, B(this.grab_date.toString))
-        this.keyWords.foreach((x: (String, Int)) =>
-            dbclient.zadd("pages:KW:" + x._1, x._2, B(this.URL))
-            //dbclient.set(page_key + "KW:" + x._1, B(x._2))
-        )
-    }
-}
-
-
-object CrawlerSupport {
-    def get_splited_page(s: String) = {
-        new java.util.StringTokenizer(s.toLowerCase, " \n,.:;\"'/\\+-=!@#$%^&*|_()<>{}[]~`\t", true)
+  def saveIntoDB(databaseClient: Redis) {
+    val index = {
+      val existingKeys = databaseClient.keys("pages:*:url:" + url)
+      if (existingKeys.length > 0) existingKeys(0).split(":")(1)
+      else {
+        val res = S(databaseClient get "pages:globalindex").get
+        databaseClient incr "pages:globalindex"
+        res
+      }
     }
 
-    def md5(s: String) = {
-        MessageDigest.getInstance("MD5").digest(s.getBytes)
+    println("index:" + index)
+    val pageKey = "pages:" + index + ":"
+    println(pageKey + "url:" + url)
+    databaseClient.set(pageKey + "url:" + url, B(grabDate.toString))
+    keyWords foreach {
+      case (k, v) => databaseClient.zadd("pages:KW:" + k, v, B(url))
     }
+    //databaseClient.set(page_key + "KW:" + x._1, B(x._2))
+  }
 }
 
 class Crawler {
 
-    def openResourceInputStream(url: String) = {
-        val connection = new URL(url).openConnection()
-        connection.setRequestProperty("User-Agent", "Mozilla/5.0 (compatible;)")
-        connection.connect()
-        connection.getInputStream
+  object CrawlerSupport {
+    def splitPage(s: String) =
+      new StringTokenizer(s.toLowerCase, " \n,.:;\"'/\\+-=!@#$%^&*|_()<>{}[]~`\t", true)
+
+    def md5(s: String) =
+      MessageDigest.getInstance("MD5").digest(s.getBytes)
+  }
+
+  val hyperLinkPattern = Pattern compile "http:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+"
+
+  def openResourceInputStream(url: String) = {
+    val connection = new URL(url).openConnection()
+    connection.setRequestProperty("User-Agent", "Mozilla/5.0 (compatible;)")
+    connection.connect()
+    connection.getInputStream
+  }
+
+  def grabUrl(url: String) = {
+    val rawPage = Source.fromInputStream(openResourceInputStream(url)).getLines().mkString("\n")
+    val hyperLinkMatcher = hyperLinkPattern.matcher(rawPage)
+
+    def getHyperLinks(links: List[String] = List()): List[String] =
+      if (hyperLinkMatcher.find()) getHyperLinks(hyperLinkMatcher.group(0) :: links)
+      else links
+
+    val keyWords = new mutable.HashMap[String, Int]()
+    val tokens = CrawlerSupport splitPage rawPage
+
+    while (tokens.hasMoreTokens) {
+      val word = tokens.nextToken()
+      if (!(keyWords contains word)) keyWords put(word, 0)
+      keyWords(word) += 1
     }
 
-    val hrefPattern = Pattern.compile( """http:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+""")
-    val imagePattern = Pattern.compile("(src|href)[^><\"]*\"([^\"\']*\\.(gif|jpg|png))\"")
-    //val bodyPattern = Pattern.compile("<script>(\\S+)</script>")
+    val (images, hyperLinks) = getHyperLinks().partition(link =>
+      (link endsWith ".jpg") || (link endsWith ".ico") || (link endsWith ".png") || (link endsWith ".gif"))
 
-    def grabUrl(url: String) = {
-        val raw_page = scala.io.Source.fromInputStream(openResourceInputStream(url)).getLines().mkString("\n") //getPage()
-        val href_matcher = hrefPattern.matcher(raw_page)
-        val image_matcher = imagePattern.matcher(raw_page)
+    new StoredPage(url, "", keyWords, hyperLinks, images, CrawlerSupport.md5(rawPage), new Date)
+  }
 
-        def getHref(res: List[String] = List[String]()): List[String] = {
-            if (href_matcher.find()) {
-                getHref(res :+ href_matcher.group(0))
+  def grabHost(majorURL: String, databaseClient: Redis, maxDepth: Int = 1) {
+    //var pages = scala.collection.mutable.HashMap[String, StoredPage]()
+    def walker(url: String, depth: Int) {
+      //if (!pages.contains(url)) {
+      if (databaseClient.keys("pages:*:URL:" + url).length == 0) {
+        val page = grabUrl(url)
+        println("walking page url " + page.URL + "  num of hrefs " + page.links.length)
+        page.saveIntoDB(databaseClient)
+        //pages.put(url, page)
+        if (depth < maxDepth)
+          page.links.filter(_ startsWith majorURL).foreach((x: String) => {
+            try {
+              walker(x, depth + 1)
+            } catch {
+              case e: Exception => println(e)
             }
-            else {
-                res
-            }
-        }
-        def getImages(res: List[String] = List[String]()): List[String] = {
-            if (image_matcher.find()) {
-                getHref(res :+ image_matcher.group(2))
-            }
-            else {
-                res
-            }
-        }
-
-        var keyWords = new scala.collection.mutable.HashMap[String, Int]() //TODO: FIX IT
-
-        val tokens = CrawlerSupport.get_splited_page(raw_page)
-        while (tokens.hasMoreTokens()) {
-            val word = tokens.nextToken()
-            if (!keyWords.contains(word)) keyWords.put(word, 0)
-            keyWords(word) += 1
-        }
-        val hrefs = getHref().filter((x: String) => !(x.endsWith(".jpg") || x.endsWith(".ico") || x.endsWith(".png") || x.endsWith(".gif")))
-        val images = getImages()
-        StoredPage(url, "", keyWords, hrefs, images, CrawlerSupport.md5(raw_page), new Date)
+          })
+      } else {
+        println("passing " + url)
+      }
     }
 
-    def grabHost(major_url: String, dbclient: Redis, max_depth: Int = 1) = {
-        //var pages = scala.collection.mutable.HashMap[String, StoredPage]()
-        def walker(url: String, depth: Int): Unit = {
-            //if (!pages.contains(url)) {
-            if (dbclient.keys("pages:*:URL:" + url).length == 0) {
-                val page = grabUrl(url)
-                println("walking page url " + page.URL + "  num of hrefs " + page.links.length)
-                page.saveIntoDB(dbclient)
-                //pages.put(url, page)
-                if (depth < max_depth)
-                    page.links.filter(_.startsWith(major_url)).foreach((x: String) => {
-                        try {
-                            walker(x, depth + 1)
-                        } catch {
-                            case e: Exception => println(e)
-                        }
-                    })
-            } else {
-                println("passing " + url)
-            }
-        }
-        walker(major_url, 0)
-        //pages
-    }
+    walker(majorURL, 0)
+    //pages
+  }
 }
