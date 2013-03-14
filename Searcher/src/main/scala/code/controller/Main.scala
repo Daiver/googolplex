6edb8b25d75b7799f4b4983be84482dbeba18385
Yau Ref <yau.ref@gmail.com>
2013-03-14 15:24:54 +0400

    Appended Main file to run SearchEngine without lift

---
+++ Searcher/src/main/scala/code/controller/Main.scala
@@ -0,0 +1,19 @@
+package code.controller
+
+import org.googolplex.SearchEngine
+import com.redis.RedisClient
+
+object Main {
+  def main(args: Array[String]) {
+    println("query> ")
+    val query = readLine()
+    search(query) foreach println
+  }
+
+  def search(query: String) = {
+    val searchEngine = new SearchEngine
+    val redisClient = new RedisClient("192.168.56.100", 6379)
+    searchEngine.search(query, redisClient).reverse
+  }
+
+}
