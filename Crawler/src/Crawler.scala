8d4e99e8c1da950021a0a63ad3df8f7e8bd5054d
Yau Ref <yau.ref@gmail.com>
2013-04-03 17:52:35 +0400

    Image hashing in crawler

--- Crawler/src/Crawler.scala
+++ Crawler/src/Crawler.scala
@@ -16,7 +16,7 @@ class StoredPage(val url: String,
                  val hash: Array[Byte],
                  val grabDate: Date) {
 
-  def saveIntoDB(databaseClient: RedisClient) {
+  def saveIntoDB(databaseClient: RedisClient) = {
     val pageKey = "page:" + url
     val pageInfo =
       if (!(databaseClient exists pageKey)) {
@@ -31,25 +31,23 @@ class StoredPage(val url: String,
 
     databaseClient.hmset(pageKey, pageInfo)
 
-    keyWords foreach {
+    keyWords.foreach {
       case (word, count) =>
         databaseClient.zadd("page:keyword:" + word, count, url)
     }
+
+    pageKey
   }
 }
 
 class Crawler {
 
-  object CrawlerSupport {
-    def splitPage(s: String) =
-      new StringTokenizer(s.toLowerCase, " \n,.:;\"'/\\+-=!@#$%^&*|_()<>{}[]~`\t", true)
-
-    def md5(s: String) =
-      MessageDigest.getInstance("MD5").digest(s.getBytes)
-  }
-
+  val md5 = MessageDigest.getInstance("MD5")
   val hyperLinkPattern = Pattern.compile( """http:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+""")
   val hardSplitters = List("\n", "\t", " ")
+
+  def splitPage(s: String) =
+    new StringTokenizer(s.toLowerCase, " \n,.:;\"'/\\+-=!@#$%^&*|_()<>{}[]~`\t", true)
 
   def openResourceInputStream(url: String) = {
     val connection = new URL(url).openConnection()
@@ -67,8 +65,7 @@ class Crawler {
       else links
 
     val keyWords = new mutable.HashMap[String, Int]()
-    val tokens = CrawlerSupport splitPage rawPage
-
+    val tokens = splitPage(rawPage)
 
     var inTag = false
     var readTitle = false
@@ -95,21 +92,33 @@ class Crawler {
 
       if (!(keyWords contains word)) keyWords put(word, 0)
       keyWords(word) += 1
-
     }
 
     val (images, hyperLinks) = getHyperLinks().partition(link =>
       (link endsWith ".jpg") || (link endsWith ".ico") || (link endsWith ".png") || (link endsWith ".gif"))
     println("title! " + title)
-    new StoredPage(url, title, "", keyWords, hyperLinks, images, CrawlerSupport.md5(rawPage), new Date)
+    new StoredPage(url, title, "", keyWords, hyperLinks, images, md5.digest(rawPage.getBytes), new Date)
   }
 
   def grabHost(majorURL: String, databaseClient: RedisClient, maxDepth: Int = 1) {
+
+    def saveImages(page: StoredPage, pageId: String) = {
+      page.images.foreach((x: String) => {
+        try {
+          val hash = LooksLike.hashImage(x)
+          //databaseClient.push
+        } catch {
+          case e: Exception => println(e)
+        }
+      })
+    }
+
     def walker(url: String, depth: Int) {
       if (!(databaseClient exists ("page:" + url))) {
         val page = grabUrl(url)
-        println("walking page url " + page.url + "  num of hrefs " + page.links.length)
-        page.saveIntoDB(databaseClient)
+        val pageId = page.saveIntoDB(databaseClient)
+        println("walking page url " + page.url + "  number of links" + page.links.length)
+        saveImages(page, pageId)
         if (depth < maxDepth)
           page.links.filter(_ startsWith majorURL).foreach((x: String) => {
             try {
@@ -118,6 +127,7 @@ class Crawler {
               case e: Exception => println(e)
             }
           })
+
       } else {
         println("passing " + url)
       }
@@ -126,4 +136,41 @@ class Crawler {
     walker(majorURL, 0)
     println("Number of crawled pages: " + (databaseClient get "pages:globalindex").get)
   }
+}
+
+object LooksLike {
+
+  import java.awt.image.BufferedImage
+  import javax.imageio.ImageIO
+
+  def hashImage(imageUrl: String): BigInt = {
+    val image = ImageIO.read(f)
+    val hash = hashImage(image)
+  }
+
+  def hashImage(image: BufferedImage): BigInt = {
+    def foldPixels(image: BufferedImage, f: (BigInt, Int) => BigInt) = {
+      val w = image.getWidth
+      val h = image.getHeight
+      def fold(i: Int, j: Int, acc: BigInt): BigInt =
+        if (i == h) acc
+        else if (j == w) fold(i + 1, 0, acc)
+        else fold(i, j + 1, f(acc, image.getRGB(i, j)))
+
+      fold(0, 0, 0)
+    }
+    val normalizedImage = {
+      val w = 8
+      val h = w
+      val scaledGrayImage = new BufferedImage(w, h, BufferedImage.TYPE_BYTE_GRAY)
+      scaledGrayImage.createGraphics().drawImage(image, 0, 0, w, h, null)
+      scaledGrayImage
+    }
+    val imageSize = normalizedImage.getWidth * normalizedImage.getHeight
+    val averagePixelColor = foldPixels(normalizedImage, (s, c) => s + c) / imageSize
+    def hash(h: BigInt, c: Int) = (h << 1) + (if (c < averagePixelColor) 0 else 1)
+
+    foldPixels(normalizedImage, hash)
+  }
+
 }
